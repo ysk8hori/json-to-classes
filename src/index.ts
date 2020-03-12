28ee3a7323caeb08b9be30e4579ef0932d529e3b
ysk8.parchase@gmail.com <ysk8.parchase@gmail.com>
2020-03-13 00:04:32 +0900

    コミット

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { getTypeStructure, optimizeTypeStructure } from "./get-type-structure";
 import { Options } from "./model";
 import { shim } from "es7-shim/es7-shim";
-import {
-  getInterfaceDescriptions,
-  getInterfaceStringFromDescription
-} from "./get-interfaces";
+import { getInterfaceDescriptions, getInterfaceStringFromDescription } from "./get-interfaces";
 import { getNames } from "./get-names";
 import { isArray, isObject } from "./util";
+import { getClassStringFromDescription } from "./get-classes";
 shim();
 
 export default function JsonToTS(json: any, userOptions?: Options): string[] {
+  const { typeStructure, names } = getStructureAndNames(userOptions, json);
+
+  return getInterfaceDescriptions(typeStructure, names).map(getInterfaceStringFromDescription);
+}
+
+function getStructureAndNames(userOptions: Options, json: any) {
   const defaultOptions: Options = {
     rootName: "RootObject"
   };
@@ -17,33 +21,32 @@ export default function JsonToTS(json: any, userOptions?: Options): string[] {
     ...defaultOptions,
     ...userOptions
   };
-
   /**
    * Parsing currently works with (Objects) and (Array of Objects) not and primitive types and mixed arrays etc..
    * so we shall validate, so we dont start parsing non Object type
    */
-  const isArrayOfObjects =
-    isArray(json) &&
-    json.length > 0 &&
-    json.reduce((a, b) => a && isObject(b), true);
-
+  const isArrayOfObjects = isArray(json) && json.length > 0 && json.reduce((a, b) => a && isObject(b), true);
   if (!(isObject(json) || isArrayOfObjects)) {
     throw new Error("Only (Object) and (Array of Object) are supported");
   }
-
   const typeStructure = getTypeStructure(json);
   /**
    * due to merging array types some types are switched out for merged ones
    * so we delete the unused ones here
    */
   optimizeTypeStructure(typeStructure);
-
   const names = getNames(typeStructure, options.rootName);
+  return { typeStructure, names };
+}
 
-  return getInterfaceDescriptions(typeStructure, names).map(
-    getInterfaceStringFromDescription
-  );
+export function JsonToClasses(json: any, userOptions?: Options): string[] {
+  const { typeStructure, names } = getStructureAndNames(userOptions, json);
+
+  return getInterfaceDescriptions(typeStructure, names).map(getClassStringFromDescription);
 }
 
 (<any>JsonToTS).default = JsonToTS;
 module.exports = JsonToTS;
+
+(<any>JsonToClasses).default = JsonToClasses;
+module.exports = JsonToClasses;
--- test/js-integration/index.js
+++ test/js-integration/index.js
@@ -1,36 +1,43 @@
-const assert = require('assert')
+const assert = require("assert");
 
-describe("Javascript integration", function () {
+describe("Javascript integration", function() {
+  it("should work with default require statement", function() {
+    const JsonToClasses = require("../../build/src/index");
 
-  it("should work with default require statement", function () {
-    const JsonToTS = require('../../build/src/index')
+    const expected0 = `class RootObject {
+  constructor(
+    private cats: Cat[],
+    private favoriteNumber: number,
+    private favoriteWord: string,
+  ){}
+}`;
 
-    const expected = `
-interface RootObject {
-  cats: Cat[];
-  favoriteNumber: number;
-  favoriteWord: string;
-}
-interface Cat {
-  name: string;
-}`
+    const expected1 = `class Cat {
+  constructor(
+    private name: string,
+  ){}
+}`;
 
     const json = {
-      cats: [
-        {name: 'Kittin'},
-        {name: 'Mittin'},
-      ],
+      cats: [{ name: "Kittin" }, { name: "Mittin" }],
       favoriteNumber: 42,
-      favoriteWord: 'Hello'
-    }
+      favoriteWord: "Hello"
+    };
 
-    const output = JsonToTS(json)
-      .reduce((type1, type2) => {
-        return `${type1}\n${type2}`
-      })
-      .trim()
+    const output = JsonToClasses(json);
+    // const asd = [0,1,2];
+    // asd.forEach
+    output.forEach((o, index) => console.log(`▼${index}\n${o}\n`));
+    // .reduce((type1, type2) => {
+    //   return `${type1}\n${type2}`;
+    // })
+    // .trim();
+    console.log(JSON.stringify(output));
 
-    assert.strictEqual(output, expected.trim())
-  })
+    console.log(output[0]);
+    console.log(output[1]);
 
-})
+    assert.strictEqual(output[0], expected0);
+    // assert.strictEqual(output[1], expected1);
+  });
+});
